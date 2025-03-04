c345518bdb314202af268f0e1d289645d758a586
celery-xqq <1278605890@qq.com>
2025-03-04 12:09:50 +0800

    add new file

--- LearnGit/test.py
+++ LearnGit/test.py
@@ -1,3 +1,9 @@
 # -*- coding: utf-8 -*-
 print ("hello world")
 print("你好")
+
+
+def solution(A):
+    # write your code in Python 2.7
+    A.sort()
+    return max(A[0]*A[1]*A[-1],A[-1]*A[-2]*A[-3])
